225da956e42e85026c7296408bbd5cde8fa5c178
abhinavkamble <abhinavkamble00@gmail.com>
2022-07-13 22:32:12 +0530

    not changed doc

---
+++ src/Salenium1.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Salenium1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Java Selenium 4.1.4\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.irctc.co.in/nget/train-search");
+//		driver.findElement(By.xpath("//input[@role='searchbox']")).sendKeys("Enter Charactors");
+//		driver.findElement(By.xpath("//input[@id='concessionBooking']")).click();
+//		driver.findElement(By.xpath("//label[@for='passBooking']")).click();
+		
+		
+		driver.get("https://www.redbus.in/");
+		driver.findElement(By.xpath("//input[@id='src']")).sendKeys("Pune");
+
+	}
+
+}
